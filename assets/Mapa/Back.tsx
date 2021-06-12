eafacdcc671738dbeef1de16f751ba0c7106a54e
S-Baldi <santiagonbaldi@outlook.com>
2021-06-12 18:33:05 -0300

    Primer cambio

---
+++ assets/Mapa/Back.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.0" name="Back" tilewidth="32" tileheight="32" tilecount="180" columns="18">
+ <image source="Nueva carpeta (2)/free-swamp-game-tileset-pixel-art/2 Background/Layers/3.png" width="576" height="324"/>
+</tileset>
